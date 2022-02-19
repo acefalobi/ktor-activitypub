a84720d6ebe61466599d930e8842dec4bb73e249
Ace Falobi <acefalobi@gmail.com>
2022-02-19 02:27:38 +0100

    Added docker config

--- src/main/kotlin/social/aceinteract/Application.kt
+++ src/main/kotlin/social/aceinteract/Application.kt
@@ -3,25 +3,14 @@ package social.aceinteract
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import social.aceinteract.plugins.configureRouting
-import java.io.File
-import java.security.KeyStore
 
 fun main() {
-    val keyStoreFile = File("build/keystore.jks")
     val environment = applicationEngineEnvironment {
         connector {
             port = 80
             host = "0.0.0.0"
         }
         watchPaths = listOf("classes")
-        sslConnector(
-            keyStore = KeyStore.getInstance(keyStoreFile, "password".toCharArray()),
-            keyAlias = "ktorActivitypubAlias",
-            keyStorePassword = { "omolola".toCharArray() },
-            privateKeyPassword = { "omolola".toCharArray() }) {
-            port = 8443
-            keyStorePath = keyStoreFile
-        }
         module {
             configureRouting()
         }
